3daba614cd338edcaa4192bcecf8f9621a3895b7
llopez-cmnh <llopez@cmnhospitals.org>
2025-10-31 22:49:41 -0600

    fixing build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import node from '@astrojs/node';
 
 // https://docs.astro.build/en/reference/configuration-reference/
 export default defineConfig({
   integrations: [vue()],
   output: 'server',
+  adapter: node({ mode: 'standalone' }),
   server: { host: true },
 });
 
